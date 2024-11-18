f5ab8d915598d725865f8c0bab0dfe4138c02aee
dc <1090504489@qq.com>
2024-11-18 15:22:39 +0800

    修改制造冲突

--- src/components/cy.vue
+++ src/components/cy.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: dc 1090504489@qq.com
+ * @Date: 2024-11-18 15:20:20
+ * @LastEditors: dc 1090504489@qq.com
+ * @LastEditTime: 2024-11-18 15:21:11
+ * @FilePath: \vue3-git-demo\src\components\cy.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div>cy123123</div>
+  <div>123123</div>
 </template>
 
 <script setup></script>
